64d73243200bd3d6823079350bec0d4d31d56d3e
zhoujun <dev.zhoujun@gmail.com>
2017-01-01 17:25:30 +0800

    add docker-entrypoint.sh

---
+++ ss-panel/docker-entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -eo pipefail
+
+__init() {
+    git clone https://github.com/orvice/ss-panel.git .
+    curl -sS https://getcomposer.org/installer | php 
+    php composer.phar install
+
+    cp .env.example .env
+    sed -i -e "s/'md5'/'sha256'/g" .env
+    sed -i -e "s/'file'/'redis'/g" .env
+    sed -i -e "s/localhost/mysql/g" .env
+    sed -i -e "s/127.0.0.1/redis/g" .env
+}
+
+if [ "$1" == "init" ]; then
+    __init
+elif [ "$1" == "create-admin" ]; then
+    php xcat createAdmin
+elif [ "$1" == "reset-traffic" ]; then
+    php xcat resetTraffic
+elif [ "$1" == "reset-diary-mail" ]; then
+    php xcat sendDiaryMail
+elif [ "$1" == "php-fpm" ]; then
+    php-fpm
+elif [ "$1" == "bash" ]; then
+    /bin/bash
+else
+    echo "command not found: $1"
+fi
